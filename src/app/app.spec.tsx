61ec437c254be394bde0ac3e6306844c14965a7f
Saharsh <saharshg10@gmail.com>
2023-06-10 21:01:44 +0530

    Rename files to food-is-life

--- src/app/app.spec.tsx
+++ src/app/app.spec.tsx
@@ -10,6 +10,6 @@ describe('App', () => {
 
   it('should have a greeting as the title', () => {
     const { getByText } = render(<App />);
-    expect(getByText(/Welcome realtime-talks/gi)).toBeTruthy();
+    expect(getByText(/Welcome food-is-life/gi)).toBeTruthy();
   });
 });
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 import viteTsConfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  cacheDir: './node_modules/.vite/realtime-talks',
+  cacheDir: './node_modules/.vite/food-is-life',
 
   server: {
     port: 4200,
